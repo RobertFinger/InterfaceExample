c2ef926e7f6ddbfc0c2f256b6bbcf3e1f1115952
robertfinger <robertfinger@yahoo.com>
2023-09-05 11:07:53 -0400

    add duck and walrus as animal types

---
+++ InterfaceExample/Dog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    public class Dog : IAnimal
+    {
+        public bool Eat(FoodType food)
+        {
+            switch (food)
+            {
+                case FoodType.Meat:
+                    return true;
+                case FoodType.Fish:
+                    return true;
+                case FoodType.Vegetables:
+                    return false;
+                case FoodType.Fruits:
+                    return false;
+                default:
+                    return false;
+            }
+
+        }
+
+        public string MakeSound()
+        {
+            return "Bark! Bark! Bark!";
+        }
+
+        public bool Move(MovementType move)
+        {
+            switch (move)
+            {
+                case MovementType.Walk:
+                    return true;
+                case MovementType.Fly:
+                    return false;
+                case MovementType.Swim:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ InterfaceExample/Duck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    public class Duck : IAnimal
+    {
+        public bool Eat(FoodType food)
+        {
+            switch (food)
+            {
+                case FoodType.Meat:
+                    return false;
+                case FoodType.Fish:
+                    return true;
+                case FoodType.Vegetables:
+                    return true;
+                case FoodType.Fruits:
+                    return false;
+                default:
+                    return false;
+            }
+
+        }
+
+        public string MakeSound()
+        {
+            return "Quack! Quack! Quack!";
+        }
+
+        public bool Move(MovementType move)
+        {
+            switch (move)
+            {
+                case MovementType.Walk:
+                    return true;
+                case MovementType.Fly:
+                    return true;
+                case MovementType.Swim:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
