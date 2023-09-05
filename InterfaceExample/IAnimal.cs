9271d86655fa094d16cbe7c4c48a87ff2cbcdbdd
robertfinger <robertfinger@yahoo.com>
2023-09-05 11:02:52 -0400

    add interface and enums

---
+++ InterfaceExample/IAnimal.cs
@@ -0,0 +1,33 @@
+﻿namespace InterfaceExample
+{
+    public interface IAnimal
+    {
+        public string MakeSound();
+        public bool Move(MovementType move);
+        public bool Eat(FoodType food);
+
+    }
+
+    public enum AnimalType
+    {
+        Dog,
+        Cat,
+        Bird,
+        Fish
+    }
+
+    public enum FoodType
+    {
+        Meat,
+        Fish,
+        Vegetables,
+        Fruits
+    }
+
+    public enum MovementType
+    {
+        Walk,
+        Fly,
+        Swim
+    }
+}
